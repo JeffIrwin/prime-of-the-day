f8c039ec22524fa92bb50a0a9a6f6e8c94b7088a
Jeff Irwin <jirwin505@gmail.com>
2024-08-17 22:54:20 -0400

    update to checkout v3

--- run.sh
+++ run.sh
@@ -9,20 +9,23 @@ set -e
 echo "user_id = ${user_id:0:3}********"
 echo "token = ${token:0:3}********"
 
-# TODO
-exit 0
-
 #source secrets.sh
 
 # Image example:
 #
 #     curl -i -X POST "https://graph.threads.net/v1.0/USER_ID/threads" -d "media_type=IMAGE" -d "image_url=https://www.jeffirwin.xyz/favicon.png" -d "text=#BronzFonz" -d "access_token=TOKEN"
 
-# TODO: get text from running c++ program
-
-#text="now i have to figure out what to do with this. post \"it's wednesday my dudes\" every wednesday or something"
 #text="hello from ec2"
-text="13"
+#text="13"
+
+g++ -o main main.cpp
+prime=$(./main | tail -1)
+echo "prime = $prime"
+
+text="$prime"
+
+# exit here for a dry run
+exit 0
 
 # You can get the user id like this:
 #
