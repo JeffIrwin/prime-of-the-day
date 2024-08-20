9523c4ac1dbf840d0e753251c8130f24852ef191
Jeff Irwin <jirwin505@gmail.com>
2024-08-19 20:46:30 -0400

    add some unit tests

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 
-//#include <chrono>
 #include <iostream>
 #include <stdio.h>
 #include <string>
 #include <vector>
 
 #include "state.h"
+
+// ANSI escape codes
+const std::string RED    = "\033[31m";
+const std::string YELLOW = "\033[33m";
+const std::string RESET  = "\033[0m";
 
 size_t nth_prime_number(size_t n)
 {
@@ -52,7 +56,6 @@ std::string comma_delim(size_t n)
 
 	// should use str builder but these aren't very big
 	std::string str = "";
-	//for (int i = nstr - 1; i >= 0; i--)
 	for (int i = 1; i <= nstr; i++)
 	{
 		str = str_bare[nstr - i] + str;
@@ -62,30 +65,66 @@ std::string comma_delim(size_t n)
 	return str;
 }
 
-int main()
+size_t unit_tests()
 {
+	size_t nfail = 0;
+
+	// TODO: refactor as bool array
+
+	if (comma_delim(1) != "1") nfail++;
+	if (comma_delim(12) != "12") nfail++;
+	if (comma_delim(123) != "123") nfail++;
+
+	if (comma_delim(1234) != "1234") nfail++;
+	if (comma_delim(12345) != "12,345") nfail++;
+	if (comma_delim(123456) != "123,456") nfail++;
+
+	if (comma_delim(1234567) != "1,234,567") nfail++;
+	if (comma_delim(12345678) != "12,345,678") nfail++;
+	if (comma_delim(123456789) != "123,456,789") nfail++;
+
+	if (comma_delim(1234567891) != "1,234,567,891") nfail++;
+	if (comma_delim(12345678912) != "12,345,678,912") nfail++;
+	if (comma_delim(123456789123) != "123,456,789,123") nfail++;
+
+	// TODO: log if failure.  run during "push" ci/cd (but not "schedule")
+
+	// TODO: also cover nth_prime_number()
+
+	// TODO: log green if no fails
+	if (nfail != 0)
+		std::cerr
+			<< RED
+			<< "Error: " << nfail << " unit test(s) failed\n"
+			<< RESET;
+
+	return nfail;
+}
+
+int main(int argc, char* argv[])
+{
+	bool test = false;
+	for (int i = 1; i < argc; i++)
+	{
+		std::string arg = argv[i];
+		//std::cerr << "arg = " << arg << "\n";
+		if (arg == "--test")
+			test = true;
+		else
+			std::cerr
+				<< YELLOW
+				<< "Warning: bad command line arg \"" << arg << "\"\n"
+				<< RESET;
+	}
+
+	if (test)
+		return unit_tests();
+
 	//// the 100,000'th prime number is 1,299,709 and can be calculated here in
 	//// about 13 seconds.  if i continue posting every day, it will take roughly
 	//// 300 years to get to that point, so there aren't any concerns about run
 	//// time or size_t overflow.
 	//std::cout << nth_prime_number(100000) << "\n";
-
-	//// TODO: split into unit tests.  also cover nth_prime_number()
-	//std::cerr << comma_delim(1)   << "\n";
-	//std::cerr << comma_delim(12)  << "\n";
-	//std::cerr << comma_delim(123) << "\n";
-
-	//std::cerr << comma_delim(1234)   << "\n";
-	//std::cerr << comma_delim(12345)  << "\n";
-	//std::cerr << comma_delim(123456) << "\n";
-
-	//std::cerr << comma_delim(1234567) << "\n";
-	//std::cerr << comma_delim(12345678) << "\n";
-	//std::cerr << comma_delim(123456789) << "\n";
-
-	//std::cerr << comma_delim(1234567891) << "\n";
-	//std::cerr << comma_delim(12345678912) << "\n";
-	//std::cerr << comma_delim(123456789123) << "\n";
 
 	auto days = PRIME_STATE_COUNT;
 	std::cerr << "days = " << days << " days\n";
